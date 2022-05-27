203e19717c9799208604989f244173fdc836c08c
juzeka <rafaelgomesalmeida@hotmail.com>
2022-05-27 17:21:06 -0300

    adicionando documentação com swagger

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 INSTALLED_APPS += [
     'rest_framework',
+    'drf_yasg',
 ]
 
 INSTALLED_APPS += [
@@ -134,5 +135,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
 }
--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from ingredientes.views.ingredientes import IngredientesViewSet
 from etapas.views.etapas import EtapaViewSet
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API de Receitas",
+      default_version='v1',
+      description="Descrição da api",
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 
 routers = DefaultRouter()
@@ -12,4 +26,8 @@ routers.register(r'etapas', EtapaViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(routers.urls), name='api'),
+]
+urlpatterns += [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
