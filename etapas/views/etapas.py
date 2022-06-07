d5f4b83f1147a05f97c71ed79d3ea0af44cdd3bd
juzeka <rafaelgomesalmeida@hotmail.com>
2022-06-07 19:12:28 -0300

    generalizando as permissoes das viewsets

--- etapas/views/etapas.py
+++ etapas/views/etapas.py
@@ -1,14 +1,30 @@
 from rest_framework.viewsets import ModelViewSet
 from etapas.serializers import EtapaAllSerializer
 from etapas.models import Etapa
-from rest_framework.authentication import (
-    TokenAuthentication
-)
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 
 
 class EtapaViewSet(ModelViewSet):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
     queryset = Etapa.objects.all()
     serializer_class = EtapaAllSerializer
+
+    @permission_classes([AllowAny])
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @permission_classes([AllowAny])
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @permission_classes([AllowAny])
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @permission_classes([IsAuthenticated])
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @permission_classes([IsAdminUser])
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
--- ingredientes/views/ingredientes.py
+++ ingredientes/views/ingredientes.py
@@ -1,15 +1,30 @@
 from rest_framework.viewsets import ModelViewSet
 from ingredientes.serializers.ingredientes import IngredientesAllSerializers
 from ingredientes.models.ingredientes import Ingrediente
-from rest_framework.authentication import (
-    TokenAuthentication
-)
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 
 
 class IngredientesViewSet(ModelViewSet):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
     queryset = Ingrediente.objects.all()
     serializer_class = IngredientesAllSerializers
 
+    @permission_classes([AllowAny])
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @permission_classes([AllowAny])
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @permission_classes([AllowAny])
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @permission_classes([IsAuthenticated])
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @permission_classes([IsAdminUser])
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
