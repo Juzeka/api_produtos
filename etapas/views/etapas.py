efbdd24031f5bece17d5a4c4b95cbeafc3b1ada7
juzeka <rafaelgomesalmeida@hotmail.com>
2022-06-07 18:05:34 -0300

    adcionando autenticaçao e permissao em etapas e ingredientes

--- etapas/views/etapas.py
+++ etapas/views/etapas.py
@@ -1,8 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 from etapas.serializers import EtapaAllSerializer
 from etapas.models import Etapa
+from rest_framework.authentication import (
+    SessionAuthentication, BasicAuthentication
+)
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 class EtapaViewSet(ModelViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminUser]
     queryset = Etapa.objects.all()
     serializer_class = EtapaAllSerializer
--- ingredientes/views/ingredientes.py
+++ ingredientes/views/ingredientes.py
@@ -1,9 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
 from ingredientes.serializers.ingredientes import IngredientesAllSerializers
 from ingredientes.models.ingredientes import Ingrediente
+from rest_framework.authentication import (
+    SessionAuthentication, BasicAuthentication
+)
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 class IngredientesViewSet(ModelViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminUser]
     queryset = Ingrediente.objects.all()
     serializer_class = IngredientesAllSerializers
 
