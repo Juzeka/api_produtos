8fb4b8c02d04f6e71ff0fd36d6d7c753877848a9
juzeka <rafaelgomesalmeida@hotmail.com>
2022-06-07 18:36:47 -0300

    add auth token

--- etapas/views/etapas.py
+++ etapas/views/etapas.py
@@ -2,13 +2,13 @@ from rest_framework.viewsets import ModelViewSet
 from etapas.serializers import EtapaAllSerializer
 from etapas.models import Etapa
 from rest_framework.authentication import (
-    SessionAuthentication, BasicAuthentication
+    TokenAuthentication
 )
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 class EtapaViewSet(ModelViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated, IsAdminUser]
     queryset = Etapa.objects.all()
     serializer_class = EtapaAllSerializer
--- ingredientes/views/ingredientes.py
+++ ingredientes/views/ingredientes.py
@@ -2,13 +2,13 @@ from rest_framework.viewsets import ModelViewSet
 from ingredientes.serializers.ingredientes import IngredientesAllSerializers
 from ingredientes.models.ingredientes import Ingrediente
 from rest_framework.authentication import (
-    SessionAuthentication, BasicAuthentication
+    TokenAuthentication
 )
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 class IngredientesViewSet(ModelViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated, IsAdminUser]
     queryset = Ingrediente.objects.all()
     serializer_class = IngredientesAllSerializers
